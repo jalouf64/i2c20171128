b6f619a0b632db032d59fc5098185b294a2a0099
jalouf64 <jalouf@outlook.com>
2017-11-28 17:10:21 +0100

    initialisation du projet bibliotheque[B

---
+++ bibliotheque-parent/bibliotheque-web/src/test/java/fr/uni/institute/library/web/IndexITestCase.java
@@ -0,0 +1,43 @@
+
+package fr.uni.institute.library.web;
+
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebResponse;
+
+public class IndexITestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		final String ADDRESS = "http://localhost:8080/bibliotheque-web";
+
+		 	try {
+					WebConversation webConversation = new WebConversation();
+					WebResponse menuResponse = webConversation.getResponse(ADDRESS);
+					assertEquals("Hafid-UNI Institute - UNI Library - fr_FR", menuResponse.getTitle());
+				} catch (SAXException  e) {
+					fail("SAXException: " + e.getMessage());
+				} catch (IOException e) {
+					fail("IOException: " + e.getMessage());
+				}
+			
+	}
+
+}
